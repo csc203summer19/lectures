8800fc781a6034b67e46a86f17cbf3247aca530d
Christopher Siu <christopher@siu.sh>
2019-08-07 09:57:10 -0700

    Interfaces: checking runtime types.

---
+++ 03d_nim3/GreedyPlayer.java
@@ -0,0 +1,27 @@
+/**
+ * Represents a computer player in a game of Nim.
+ */
+
+import java.util.Scanner;
+
+public class GreedyPlayer implements Player
+{
+    private String name;
+
+    public GreedyPlayer(String name) {
+        this.name = name;
+    }
+
+    public int takeSticks(Pile pile) {
+        pile.remove(3);
+        return 3;
+    }
+
+    public String makeComment() {
+        return "I am Batman.";
+    }
+
+    public String toString() {
+        return name;
+    }
+}
